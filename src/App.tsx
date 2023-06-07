b1c9c710e7b3e69081b7b82d09a5242c72ad04b8
egorlagunovich <egorlagunovich@gmail.com>
2023-06-07 11:00:57 +0200

    slide logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
 function App() {
-  return <div className="text-red-100">Hi!</div>;
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const slides = [
+    { url: require("./assets/images/1.jpg") },
+    { url: require("./assets/images/2.jpg") },
+    { url: require("./assets/images/3.jpg") },
+    { url: require("./assets/images/4.jpg") },
+  ];
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const nextSlide = () => {};
+  return (
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      ></div>
+      <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactLeft size={30} onClick={prevSlide} />
+      </div>
+      <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactRight size={30} onClick={nextSlide} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
