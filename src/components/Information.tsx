52dbfced604abb88a2390f317805c32ddf6f0386
egorlagunovich <egorlagunovich@gmail.com>
2023-09-22 15:54:54 +0200

    info text fixed

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -10,29 +10,12 @@ export default function Information() {
         <img src={require("./../assets/images/me.jpg")} alt="me" />
       </div>
       <div className="w-full sm:w-1/3 flex-wrap text-xs sm:text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic saepe optio
-        eveniet? Dolor, assumenda doloribus. Ab sunt commodi officia, dolorum
-        vero veritatis totam magnam a, nobis obcaecati maiores, consectetur non
-        ipsam eligendi adipisci quisquam libero similique dignissimos labore
-        ipsa quis deleniti? Eum dolor unde corporis reprehenderit ipsum ducimus
-        iusto doloribus deserunt saepe voluptate veniam facere voluptatum
-        praesentium quidem accusamus, sint, aliquid nesciunt ab sit similique
-        magnam reiciendis, vero at et. Reprehenderit ducimus placeat adipisci
-        libero saepe id ex neque accusantium quasi nulla, nesciunt ipsa culpa
-        quidem, vel rem delectus perferendis sapiente atque? Similique,
-        consectetur. Ullam facere dolores eaque natus, recusandae veritatis
-        omnis animi, perferendis labore facilis asperiores rerum quisquam
-        inventore, maiores aut. Omnis pariatur perferendis, blanditiis tenetur
-        fugiat in aperiam corrupti sequi est distinctio reprehenderit. Explicabo
-        reprehenderit provident unde nostrum nemo aut alias sed. Pariatur
-        repudiandae error architecto sapiente incidunt temporibus, ipsam
-        aspernatur quo perspiciatis similique mollitia beatae cumque sunt quae
-        soluta reiciendis enim aperiam aut iste esse quaerat, perferendis
-        voluptatum? Laborum delectus, reprehenderit nobis perspiciatis ab
-        consequuntur libero sint illum eum error praesentium aut provident amet
-        tempora consectetur autem esse officiis accusantium harum quidem quo
-        ipsum impedit! Eius, quisquam totam provident eaque nisi aut ducimus
-        inventore aperiam unde blanditiis.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa iure
+        officiis error modi autem ducimus vero pariatur alias cupiditate rerum?
+        Laudantium incidunt, ex veniam doloribus reiciendis accusantium atque
+        repellat, placeat et velit hic eaque deserunt unde id assumenda amet
+        reprehenderit, nobis impedit error dignissimos. Sed ipsam corporis quia
+        soluta necessitatibus!
       </div>
     </div>
   );
